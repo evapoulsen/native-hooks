903ad4d1232caddb20127f64bc5399546c6a384e
Eva S. Poulsen <eva@btech.au.dk>
2022-04-21 19:44:57 +0200

    TodoList

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import TodoList from "./src/components/TodoList";
 
-const App = () => {
+const App = (props) => {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={styles.container}>
+      <TodoList />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#DDDDDD',
+  },
+});
 
 export default App;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View,  Text, StyleSheet, FlatList } from 'react-native';
 
-const TodoList = () => {
+const TodoList = (props) => {
+    const todos = [
+        { text: 'Pay bills', id: '1'},
+        { text: 'Do your homework', id: '2'},
+        { text: 'Start writing your project', id: '3'},
+        { text: 'Design your interface', id: '4'},
+        { text: 'Interview users', id: '5'},
+
+    ];
     return (
         <View style={styles.container}>
-            <Text>TodoList</Text>
+            <FlatList
+                data={todos}
+                renderItem={({ item }) => {
+                    return <Text>{item.text}</Text>
+                }}
+                keyExtractor={(todo) => todo.id}
+            >
+            </FlatList>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
+        marginTop: 50,
     },
 });
 
 export default TodoList;
+
