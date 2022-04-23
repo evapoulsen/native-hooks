f6ef965350834909842e89ffba05f11d85867394
Eva S. Poulsen <eva@btech.au.dk>
2022-04-23 12:28:11 +0200

    AddNewTodo

---
+++ src/components/AddNewTodo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const AddNewTodo = (props) => {
+    return (
+        <View>
+            <Text>Add new todo</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
+
+export default AddNewTodo;
